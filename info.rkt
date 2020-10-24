87e38831c7d10ca453b7b44a6f291ab525588d6a
Jason Chen <kbtpodifo@gmail.com>
2020-10-24 14:29:31 -0400

    Add info and dependencies to info.rkt

--- info.rkt
+++ info.rkt
@@ -1,6 +1,8 @@
 #lang info
 
 (define collection "drracket-workspaces")
+(define version "0.1")
+(define deps '("base" "drracket-plugin-lib" "gui-lib"))
 
 (define drracket-tools '(("tool.rkt")))
 (define drracket-tool-names '("Workspaces"))
